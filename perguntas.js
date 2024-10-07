11d9d79f459fa710e01235fda4efe81f5b865d75
Andrevolpatoferreira <161027300+Andrevolpatoferreira@users.noreply.github.com>
2024-10-07 07:40:53 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Títulos',
+    'Quais títulos nacionais o athletico paranaense ja ganhou?',
+    'Um brasileirão e uma copa do brasil'
+)
+
+criaCartao(
+    'Lenda',
+    'Qual jogador é a maior lenda do athletico?',
+    'Thiago Heleno o zagueiro'
+)
+
+criaCartao(
+    'Centenário',
+    'Em que ano o Brasil completou 100 anos?',
+    '2024'
+)
+
+criaCartao(
+    'Títulos internacionais',
+    'Quantos títulos internacionais o athletico ja ganhou?',
+    '3, 2 sulamericana e 1 levain cup'
+)
